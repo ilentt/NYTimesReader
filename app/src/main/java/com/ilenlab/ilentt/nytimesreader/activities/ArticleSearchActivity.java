ca01d2777a0e822acea26c4ee2f52b71cf585ed4
ilentt <ilentt@ilenlab.com>
2016-03-22 03:53:10 +0700

    update project

--- app/src/main/java/com/ilenlab/ilentt/nytimesreader/activities/ArticleSearchActivity.java
+++ app/src/main/java/com/ilenlab/ilentt/nytimesreader/activities/ArticleSearchActivity.java
@@ -1,12 +1,20 @@
 package com.ilenlab.ilentt.nytimesreader.activities;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
+import com.ilenlab.ilentt.nytimesreader.R;
+import com.ilenlab.ilentt.nytimesreader.adapters.ArticleAdapter;
 import com.ilenlab.ilentt.nytimesreader.models.Article;
 import com.ilenlab.ilentt.nytimesreader.network.ClientAPI;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
+
+import butterknife.Bind;
 
 /**
  * Created by ADMIN on 3/18/2016.
@@ -31,14 +39,14 @@ public class ArticleSearchActivity extends AppCompatActivity implements ClientAP
         }
     }
 
-    public enum Section {
+    public enum ArticleCategories {
         ARTS("Arts"),
         FASHION("Fashion & Style"),
         SPORT("Sport");
 
         private String strValue;
 
-        Section(String value) {
+        ArticleCategories(String value) {
             strValue = value;
         }
 
@@ -50,7 +58,30 @@ public class ArticleSearchActivity extends AppCompatActivity implements ClientAP
     }
 
     public static Calendar OLDEST_DATE = Calendar.getInstance();
-    
+
+    ArrayList<Article> articles;
+    ArticleAdapter articleAdapter;
+
+    ClientAPI clientAPI;
+
+    // Reducing View Boilerplate with Butterknife
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+
+    @Bind(R.id.rvArticles)
+    RecyclerView rvArticles;
+
+    @Bind(R.id.tvBeginDate)
+    TextView tvBeginDate;
+
+    @Bind(R.id.tvOrderBy)
+    TextView tvOrderBy;
+
+    String query =  "";
+    Calendar beginDate = Calendar.getInstance();
+    ArticleOrder articleOrder = ArticleOrder.NEWEST;
+    HashMap<ArticleCategories, Boolean> categories = new HashMap<>();
+
     @Override
     public void onNewArticles(ArrayList<Article> articles) {
 
